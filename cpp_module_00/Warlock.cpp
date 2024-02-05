6674f6756d7af6a8c565cfc2f7ff019fa23793e8
Pomprab BUREERAK <pbureera@paul-f4Br4s5.clusters.42paris.fr>
2024-02-05 15:07:53 +0100

    OK

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -1,47 +1,33 @@
 #include "Warlock.hpp"
 
-Warlock::Warlock()
+Warlock::Warlock(std::string const &name, std::string const &title)
 {
-}
-
-Warlock::Warlock(Warlock const & obj)
-{
-	*this = obj;
-}
-
-Warlock &Warlock::operator=(Warlock const &op)
-{
-	this->_name = op._name;
-	this->_title = op._title;
-	return *this;
-}
-
-Warlock::Warlock(std::string name, std::string title): _name(name), _title(title)
-{
-	std::cout << _name << ": This looks like another boring day." << std::endl;
+	this->_name = name;
+	this->_title = title;
+	std::cout << this->_name << ": This looks like another boring day." << std::endl;
 }
 
 Warlock::~Warlock()
 {
-	std::cout << _name << ": My job here is done!" << std::endl;
+	std::cout << this->_name << ": My job here is done!" << std::endl;
 }
 
 std::string	const &Warlock::getName() const
 {
-	return(_name);
+	return(this->_name);
 }
 
 std::string const &Warlock::getTitle() const
 {
-	return(_title);
+	return(this->_title);
 }
 
 void	Warlock::setTitle(std::string const &str)
 {
-	_title = str;
+	this->_title = str;
 }
 
 void	Warlock::introduce()const
 {
-	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+	std::cout << this->_name << ": I am " << _name << ", " << this->_title << "!" << std::endl;
 }
--- cpp_module_00/Warlock.hpp
+++ cpp_module_00/Warlock.hpp
@@ -1,21 +1,20 @@
 #pragma once
 
-# include <iostream>
+#include <iostream>
 
 class Warlock
 {
-	private:
-		Warlock();
-		Warlock(Warlock const & obj);
-		Warlock &operator=(Warlock const &op);
-		std::string	_name;
-		std::string	_title;
-	
-	public:
-		Warlock(std::string name, std::string title);
-		~Warlock();
-		std::string	const &getName() const;
-		std::string const &getTitle() const;
-		void	setTitle(std::string const &str);
-		void	introduce() const;
+    private:
+        Warlock();
+        Warlock(Warlock const &obj);
+        Warlock &operator=(Warlock const &op);
+        std::string _name;
+        std::string _title;
+    public:
+        Warlock(std::string const &name, std::string const &title);
+        ~Warlock();
+        std::string const &getName() const;
+        std::string const &getTitle() const;
+        void setTitle(std::string const &title);
+        void introduce() const;
 };
